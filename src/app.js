81830a1c8ca3194081effc890adddb12bcaa8a6c
Lnikita123 <nikitalilhore1902@gmail.com>
2025-06-02 23:26:26 +0530

    ++changes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { setupHelmet } = require('./middlewares/helmetSetup');
 const { setupCsrf } = require('./middlewares/csrfProtection');
-
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 const sanitizeInputs = require('./middlewares/inputSanitizer');
 
 const { ipBlacklist } = require('./middlewares/ipBlacklist');
 
 
-
+// ... other middleware and routes
 
 
 const publicRoutes = require('./routes/publicRoutes');
@@ -17,7 +18,7 @@ const adminRoutes = require('./routes/adminRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const app = express();
 
-
+// Setup security headers
 
 // Setup CSRF protection globally for POST, PUT, DELETE
 setupCsrf(app);
@@ -28,13 +29,19 @@ app.use(sanitizeInputs);  // sanitize before CSRF
 
 
 setupCsrf(app);           // CSRF protection middleware
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
 
-
+app.use('/api/form', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken()); // Send token as cookie
+    res.json({ csrfToken: req.csrfToken() });  // Optional
+});
+// your routes here
 
 // Routes
 app.use('/api', publicRoutes);
 app.use('/api', submitRoutes);
-app.use('/api', adminRoutes);
+app.use('/api/admin', adminRoutes);
 app.use('/api', contactRoutes);
 
 // Global error handler for CSRF errors
